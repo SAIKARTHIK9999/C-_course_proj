3affe2c5cfc09be38b3e5184e26bc8678b54b9b4
karthik <67945756+SAIKARTHIK9999@users.noreply.github.com>
2023-02-03 14:12:31 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,67 @@
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/objdetect.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+/*
+void main() {
+	string path = "C:/Users/karth/source/repos/Project1/Project1/Resources/test.png";
+	Mat	img = imread(path);
+	imshow("Frame", img);
+	waitKey(0);
+}
+*/
+/*
+void main() {
+	VideoCapture video(1);
+	CascadeClassifier facedetect;
+	Mat img;
+	facedetect.load("haarcascade_frontalface_default.xml");
+	
+	while (true) {
+		video.read(img);
+
+		vector<Rect> faces;
+
+		facedetect.detectMultiScale(img, faces, 1.3, 5);
+
+		cout << faces.size() << endl;
+
+		for (int i = 0; i < faces.size(); i++) {
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(50, 50, 255), 3);
+			rectangle(img, Point(0,0), Point(250,70), Scalar(50, 50, 255), FILLED);
+			putText(img, to_string(faces.size())+" Face Found", Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(255, 255, 255), 1);
+		}
+
+		
+		imshow("Frame", img);
+		waitKey(0);
+		
+
+	}
+}*/
+void main() {
+
+	string path = "C:/Users/karth/source/repos/Project1/Project1/Resources/test.png";
+	Mat img = imread(path);
+
+	CascadeClassifier faceCascade;
+	faceCascade.load("C:/Users/karth/source/repos/Project1/Project1/Resources/haarcascade_frontalface_default.xml");
+
+	if (faceCascade.empty()) {
+		cout << "XML file not Loaded" << endl;
+	}
+	vector<Rect> faces;
+	faceCascade.detectMultiScale(img, faces, 1.1, 10);
+
+	for (int i = 0; i < faces.size(); i++) {
+		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+	}
+
+	imshow("Image", img);
+	waitKey(0);
+
+}
